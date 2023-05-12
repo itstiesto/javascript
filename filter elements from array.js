4e85a2339f155c2ebe45e128b6d80fb957ea1cb9
Saurabh Mishra <97050244+itstiesto@users.noreply.github.com>
2023-05-13 03:32:52 +0530

    Create filter elements from array.js

---
+++ filter elements from array.js
@@ -0,0 +1,11 @@
+var filter = function(arr, fn) {
+   let newArray = [];
+   let i=0;
+   for(const x of arr){
+      if(fn(x,i)){
+         newArray.push(x);
+      }
+      i++;
+   }
+   return newArray;
+};
